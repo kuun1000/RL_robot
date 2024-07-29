9643dcd906a22a3facdfb3b157aa77d6fa77523b
lys <lys@ketiict.com>
2024-07-29 16:59:25 +0900

    Frame of Envrionment Class

---
+++ xArm6_PickandPlace_Env.py
@@ -0,0 +1,34 @@
+import gymnasium as gym
+from gymnasium import spaces
+
+import pybullet as p
+import pybullet_data
+
+import numpy as np
+
+
+
+class xArm6GraspEnv(gym.Env):
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def _get_observation(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def _compute_reward(self, observation):
+        pass
+
+    def _is_done(self, observation):
+        pass
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
