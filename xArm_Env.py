9919ed86517462dbeacf6a63bac68e8cd7d3a271
ok yunjeong <145263884+yevettee@users.noreply.github.com>
2024-08-25 20:17:40 +0900

    Update xArm_Env.py

--- xArm_Env.py
+++ xArm_Env.py
@@ -146,8 +146,6 @@ class xArm6GraspEnv(gym.Env):
 
 
     def step(self, action):
-        pos_actions = np.linspace(-1.0, 1.0, self.num_pos_actions)
-        rot_actions = np.linspace(-np.pi, np.pi, self.num_rot_bins)
         
         # Apply action
         end_effector_pos_delta = action['end_effector_position']
