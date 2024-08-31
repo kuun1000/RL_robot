c3dac00b311e808809450456f2c6792478ca8289
ok yunjeong <145263884+yevettee@users.noreply.github.com>
2024-08-31 11:31:09 +0900

    Update xArm_Env.py

--- xArm_Env.py
+++ xArm_Env.py
@@ -6,37 +6,31 @@ import pybullet_data
 
 import numpy as np
 import cv2
+from typing import Any, Dict, Iterator, Optional
 
 
-class xArm6GraspEnv(gym.Env):
+class xArmEnv(gym.Env):
     def __init__(self):
-        super(xArm6GraspEnv, self).__init__()
+        super(xArmEnv, self).__init__()
 
-        # 관찰 공간 정의: RGB-D image, joint angles
-        self.height, self.width, self.channel = 480, 640, 4  # TODO: 크기 조절해야 함
-        self.num_joints = 7
-
+        # 관찰 공간 정의: RGB image, Depth image, Joint state
+        
+        self.height, self.width, self.channel = 480, 640, 3     # 이미지 해상도 설정 -> TODO: 해상도 조절 필요
+        self.num_joints = 6     # End-effector 제외한 관절 개수
 
         self.observation_space = spaces.Dict({
-            'rgbd': spaces.Box(low=0, high=255, shape=(self.height, self.width, self.channel), dtype=np.uint8),
-            'joint_angles': spaces.Box(low=-np.pi, high=np.pi, shape=(self.num_joints, ), dtype=np.float32)
+            'rgb': spaces.Box(low=0, high=255, shape=(self.height, self.width, self.channel), dtype=np.uint8),
+            'depth': spaces.Box(low=0, high=255, shape=(self.height, self.width, 1), dtype=np.uint8),
+            'joint_state': spaces.Box(low=-np.pi, high=np.pi, shape=(self.num_joints, ), dtype=np.float32)
         })
-
-        # # 행동 공간 정의: End-effector displacement(x, y, z), rotation(z), gripper action(closing)
-        # self.action_space = spaces.Dict({
-        #     'end_effector_position': spaces.Box(low=np.array([-1.0, -1.0, -1.0]), high=np.array([1.0, 1.0, 1.0]), dtype=np.float32),
-        #     'end_effector_rotation': spaces.Box(low=np.array([-np.pi]), high=np.array([np.pi]), dtype=np.float32),
-        #     'gripper_action': spaces.Box(low=0.0, high=1.0, shape=(1, ), dtype=np.float32),
-        # })
-
 
         # 행동 공간 정의: End-effector displacement(x, y, z), rotation(z), gripper action(closing)
         self.num_pos_actions = 5  # 예시
         self.num_rot_actions = 4  # 예시
         self.num_grip_actions = 2  # open/close 2개
-
+        
         self.action_space = spaces.Dict({
-            'end_effector_position': spaces.Discrete([self.num_pos_actions] * 3),
+            'end_effector_position': spaces.MultiDiscrete([self.num_pos_actions] * 3),
             'end_effector_rotation': spaces.Discrete(self.num_rot_actions),
             'gripper_action': spaces.Discrete(self.num_grip_actions),
         })
@@ -54,18 +48,20 @@ class xArm6GraspEnv(gym.Env):
 
         p.resetSimulation(self.client)
         p.setGravity(0, 0, -9.8, self.client)
-        
-        # 테이블 로드
-        self.table_id = p.loadURDF("table/table.urdf", basePosition=[0, 0, 0], physicsClientId=self.client)
-        # self.plane_id = p.loadURDF("plane100.urdf", basePosition=[0, 0, 0.61], physicsClientId=self.client)
-        
-        
-        # 로봇 초기 위치 및 오리엔테이션 설정
+
+        # 테이블 및 로봇 로드
+        self.table_id = p.loadURDF("table/table.urdf", 
+                                   basePosition=[0, 0, 0], 
+                                   baseOrientation=p.getQuaternionFromEuler([0, 0, np.pi/2]), 
+                                   physicsClientId=self.client)
         robot_base_position = [-0.45, 0.00, 0.60]
         robot_base_orientation = p.getQuaternionFromEuler([0, 0, -np.pi/2])
         self.robot_id = p.loadURDF("lite_6_robotarm.urdf", basePosition=robot_base_position, baseOrientation=robot_base_orientation, useFixedBase=True)
         
-        self.ee = 6     # end-effector
+        p.setCollisionFilterPair(self.robot_id, self.table_id, -1, -1, 1)
+        p.setCollisionFilterPair(self.robot_id, self.robot_id, -1, -1, 1)
+        
+        self.ee = 6
         self.camera = 9
 
         # 큐브 초기화
@@ -75,15 +71,13 @@ class xArm6GraspEnv(gym.Env):
         return self._get_observation()
 
 
-
     def reset_cube(self):
         if self.cube_id is not None:
             p.removeBody(self.cube_id, self.client)
 
-        pos = [np.random.uniform(-0.2, 0.1), np.random.uniform(0.05, 0.25), 0.65]     # 수정함
+        pos = [np.random.uniform(-0.2, 0.1), np.random.uniform(0.05, 0.25), 0.65]
         orientation = p.getQuaternionFromEuler([0, 0, 0])
         self.cube_id = p.loadURDF("cube_small.urdf", pos, orientation, physicsClientId=self.client)
-
 
 
     def arm_camera(self):
@@ -94,7 +88,7 @@ class xArm6GraspEnv(gym.Env):
         fov, aspect, near, far = 60, self.width/self.height, 0.01, 15
         projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
 
-        rot_matrix = p.getMatrixFromQuaternion(com_o)
+        rot_matrix = p.getMatrixFromQuaternion(com_o) 
         rot_matrix = np.array(rot_matrix).reshape(3, 3)
 
         # Initial vectors
@@ -117,6 +111,9 @@ class xArm6GraspEnv(gym.Env):
         depth_opengl = np.reshape(img[3], (self.height, self.width))
         depth_img_normalized = cv2.normalize(depth_opengl, None, 0, 255, cv2.NORM_MINMAX)
         depth_img = np.uint8(depth_img_normalized)
+
+        # segmentation_image = np.array(img[4]).reshape((self.height, self.width))
+        # seg_img = cv2.applyColorMap(np.uint8(segmentation_image * 255 / segmentation_image.max()), cv2.COLORMAP_JET)
 
         return rgb_img, depth_img
 
@@ -145,13 +142,14 @@ class xArm6GraspEnv(gym.Env):
 
 
     def step(self, action):
+        # Apply action
         pos_delta = np.linspace(-1.0, 1.0, self.num_pos_actions)
         end_effector_pos_delta = np.array([pos_delta[action['end_effector_position'][i]] for i in range(3)])
     
         rot_delta = np.linspace(-np.pi, np.pi, self.num_rot_actions)
         end_effector_rot_delta = rot_delta[action['end_effector_rotation']]
 
-        # Apply action
+        
         end_effector_pos_delta = action['end_effector_position']
         end_effector_rot_delta = action['end_effector_rotation']
         gripper_action = action['gripper_action'][0]
@@ -184,24 +182,10 @@ class xArm6GraspEnv(gym.Env):
 
         p.stepSimulation(self.client)
 
-        # 추가함 Check for collision with the table
-        contact_points = p.getContactPoints(bodyA=self.robot_id, bodyB=self.table_id)
-        if contact_points:
-        # If there is a collision, revert to the previous position
-            new_pos = prev_end_effector_pos
-            jointPoses = p.calculateInverseKinematics(self.robot_id, self.ee, new_pos, new_orn)
-            for i in range(self.num_joints):
-                p.setJointMotorControl2(bodyIndex=self.robot_id,
-                                    jointIndex=i,
-                                    controlMode=p.POSITION_CONTROL,
-                                    targetPosition=jointPoses[i],
-                                    )
-            p.stepSimulation(self.client)
-
         # Current position of end-effector
         cur_end_effector_pos = p.getLinkState(self.robot_id, self.ee)[0]
 
-        cube_pos = p.getBasePositionAndOrientation(self.cube_id)[0]
+        cube_pos = p.getBasePositionAndOrientation(self.cube_object_id)[0]
         # print(f"Gripper Position: {cur_end_effector_pos}, Cube Position: {cube_pos}")
 
         obs = self._get_observation()
@@ -215,7 +199,7 @@ class xArm6GraspEnv(gym.Env):
     def _compute_reward(self, observation, initial_pos, final_pos, target_pos):
         cube_pos = observation['cube_position']
 
-        gripper_contact = p.getContactPoints(bodyA=self.robot_id, bodyB=self.cube_id)
+        gripper_contact = p.getContactPoints(bodyA=self.robot_id, bodyB=self.cube_object_id)
         # print(f'gripper contact: {gripper_contact}')
         reward = 0.0
         
@@ -242,7 +226,6 @@ class xArm6GraspEnv(gym.Env):
 
     def render(self, mode='human'):
         self.arm_camera()
-
 
 
     def close(self):
